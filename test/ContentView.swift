d225cc3b7ac38dd3f9a0855594eb6db434634f73
yossy1zip <yossy@yiorno.com>
2022-06-03 17:06:21 +0900

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  test
+//  
+//  Created by yossy1zip on 2022/06/03
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,18 @@
+//
+//  testApp.swift
+//  test
+//  
+//  Created by yossy1zip on 2022/06/03
+//  
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
